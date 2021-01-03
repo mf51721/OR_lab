5c79db76812c03d830341227e01023961729a3f1
Marko Filipović <marko.filipovic@gmail.com>
2021-01-03 16:47:41 +0100

    Add Creator service as a mediator for database calls

---
+++ goapi/services/creator_service.go
@@ -0,0 +1,79 @@
+package services
+
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+
+	"github.com/mf51721/OR_lab/goapi/models"
+)
+
+type CreatorService interface {
+	Add(creator *models.Creator) error
+	Delete(id uint) error
+	Get(id uint) (*models.Creator, error)
+	GetAll(params string) (*[]models.Creator, error)
+	Update(id uint, payload map[string]interface{}) error
+}
+
+type CreatorServiceImpl struct {
+	db *gorm.DB
+}
+
+func NewCreatorService(db *gorm.DB) CreatorService {
+	return &CreatorServiceImpl{db: db}
+}
+
+// Add - Add a new programming language creator to the collection
+func (s *CreatorServiceImpl) Add(creator *models.Creator) error {
+	return s.db.Create(&creator).Error
+}
+
+// Delete - Deletes a creator entry
+func (s *CreatorServiceImpl) Delete(id uint) error {
+	// Replace with s.db.Unscoped().Delete to hard delete an entry instead of a soft delete
+	result := s.db.Delete(&models.Creator{}, id)
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected <= 0 {
+		return fmt.Errorf("no creator entries were deleted")
+	}
+	return nil
+}
+
+// Get - Find creator by Id
+func (s *CreatorServiceImpl) Get(id uint) (*models.Creator, error) {
+	var l models.Creator
+	err := s.db.Preload(clause.Associations).First(&l, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return &l, nil
+}
+
+// GetAll -
+func (s *CreatorServiceImpl) GetAll(params string) (*[]models.Creator, error) {
+	var res []models.Creator
+	err := s.db.Preload(clause.Associations).Find(&res).Error
+	if err != nil {
+		return nil, err
+	}
+	return &res, nil
+}
+
+// Update - Updates a programming language creator entry using form data
+func (s *CreatorServiceImpl) Update(id uint, payload map[string]interface{}) error {
+	lang := models.Creator{
+		Model: gorm.Model{ID: id},
+	}
+	result := s.db.Model(&lang).Updates(payload)
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected <= 0 {
+		return fmt.Errorf("no creator entries were updated")
+	}
+	return nil
+}
--- goapi/services/language_service.go
+++ goapi/services/language_service.go
@@ -25,16 +25,15 @@ func NewLanguageService(db *gorm.DB) LanguageService {
 	return &LanguageServiceImpl{db: db}
 }
 
-// AddLanguage - Add a new programming language to the collection
+// Add - Add a new programming language to the collection
 func (s *LanguageServiceImpl) Add(language *models.Language) error {
-	s.db.Create(&language)
-	return nil
+	return s.db.Create(&language).Error
 }
 
-// DeleteLang - Deletes a language entry
+// Delete - Deletes a language entry
 func (s *LanguageServiceImpl) Delete(id uint) error {
 	// Replace with s.db.Unscoped().Delete to hard delete an entry instead of a soft delete
-	result := s.db.Unscoped().Delete(&models.Language{}, id)
+	result := s.db.Delete(&models.Language{}, id)
 	if result.Error != nil {
 		return result.Error
 	}
@@ -44,7 +43,7 @@ func (s *LanguageServiceImpl) Delete(id uint) error {
 	return nil
 }
 
-// GetLang - Find language by Id
+// Get - Find language by Id
 func (s *LanguageServiceImpl) Get(id uint) (*models.Language, error) {
 	var l models.Language
 	err := s.db.Preload(clause.Associations).First(&l, id).Error
@@ -54,7 +53,8 @@ func (s *LanguageServiceImpl) Get(id uint) (*models.Language, error) {
 	return &l, nil
 }
 
-// LanguagesGet -
+// GetAll -
+// TODO: Add query parameters
 func (s *LanguageServiceImpl) GetAll(params string) (*[]models.Language, error) {
 	var res []models.Language
 	err := s.db.Preload(clause.Associations).Find(&res).Error
@@ -64,7 +64,7 @@ func (s *LanguageServiceImpl) GetAll(params string) (*[]models.Language, error) 
 	return &res, nil
 }
 
-// UpdateLangWithForm - Updates a programming language entry using form data
+// Update - Updates a programming language entry using form data
 func (s *LanguageServiceImpl) Update(id uint, payload map[string]interface{}) error {
 	lang := models.Language{
 		Model: gorm.Model{ID: id},
