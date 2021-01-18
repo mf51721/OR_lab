5b3bd1ae9c5614cadb2336d402830f1d7440daf3
Marko Filipović <marko.filipovic@gmail.com>
2021-01-18 23:43:23 +0100

    Remove unused code

--- goapi/services/language_service.go
+++ goapi/services/language_service.go
@@ -130,40 +130,6 @@ func (s *LanguageServiceImpl) GetPic(id uint) {
 	return
 }
 
-func find(obj interface{}, key string) (interface{}, bool) {
-	//if the argument is not a map, ignore it
-	mobj, ok := obj.(map[string]interface{})
-	if !ok {
-		return nil, false
-	}
-
-	for k, v := range mobj {
-		//key match, return value
-		if k == key {
-			return v, true
-		}
-
-		//if the value is a map, search recursively
-		if m, ok := v.(map[string]interface{}); ok {
-			if res, ok := find(m, key); ok {
-				return res, true
-			}
-		}
-		//if the value is an array, search recursively
-		//from each element
-		if va, ok := v.([]interface{}); ok {
-			for _, a := range va {
-				if res, ok := find(a, key); ok {
-					return res, true
-				}
-			}
-		}
-	}
-
-	//element not found
-	return nil, false
-}
-
 // GetAll -
 // TODO: Add query parameters
 func (s *LanguageServiceImpl) GetAll(params string) (*[]models.Language, error) {
